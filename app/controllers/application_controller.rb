41118e8f7e1aad7215d71b38ff464e1fe1ad8e01
arseniy96 <arsenzhar@gmail.com>
2016-11-19 22:05:08 +0300

    Add page 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
   end
 
+  private
+
+  def render_403
+    render file: "public/403.html", status: 403
+  end
+
+  def render_404
+    render file: "public/404.html", status: 404
+  end
+
+
 end
